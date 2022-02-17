0f91f847436eefa1e0a0b147a59a606df2567648
Philippe Ngom <philngom@gmail.com>
2022-02-16 17:46:34 -0800

    test passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ListItemForm from './ListItemForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders learn react link', async () => {
+  render(<ListItemForm />);
+  const linkElement = await screen.findByText(/name/i);
   expect(linkElement).toBeInTheDocument();
 });
