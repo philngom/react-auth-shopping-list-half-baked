031f1467522b3026745f3cb4051ad97b1505acfd
Philippe Ngom <philngom@gmail.com>
2022-02-16 16:32:11 -0800

    changed item prop name to listItem

--- src/ListPage.js
+++ src/ListPage.js
@@ -30,8 +30,8 @@ export default function ListPage() {
       <div className='item-list'>
         {/* map through all the list items and render them here */}
         {
-          listItems.map((item, i) =>
-            <ListItem key={item + i} item={ item }/>
+          listItems.map((listItem, i) =>
+            <ListItem key={listItem + i} listItem={ listItem }/>
           )
         }
       </div>
