7b1bbe3eee13e2da5d2c90d9f7db7dcfa8bb8dbc
Philippe Ngom <philngom@gmail.com>
2022-02-16 16:07:17 -0800

    implemented conditionals for page renders in App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,16 @@ import './App.css';
 
 export default function App() {
   // track the user in state
+  const [user, setUser] = useState('');
 
   // add a useEffect to get the user and inject the user object into state on load
+  useEffect(() => {
+    async function fetchUser() {
+      const loggedInUser = await getUser();
+      setUser(loggedInUser);
+    }
+    fetchUser();
+  }, []);
 
   async function handleLogout() {
     // call the logout function
@@ -32,9 +40,19 @@ export default function App() {
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              {
+                user
+                  ? <Redirect to='/shopping-list'/>
+                  : <AuthPage />
+              }
             </Route>
             <Route exact path="/shopping-list">
               {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
+              {
+                user
+                  ? <ListPage />
+                  : <AuthPage />
+              }
 
             </Route>
           </Switch>
