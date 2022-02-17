4571cf2fb15ff7e8700987e26908c949b3255e78
Philippe Ngom <philngom@gmail.com>
2022-02-16 16:21:51 -0800

    added useEffect on ListPage to fetch shopping list items from supabase on page load

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ export default function App() {
     <Router>
       <div className='App'>
         <header>
-          {/* if there's a user, render a logout button here */}
           {
             user && <button onClick={ handleLogout }>Logout</button>
           }
@@ -44,7 +43,6 @@ export default function App() {
         <main>
           <Switch>
             <Route exact path="/">
-              {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
               {
                 user
                   ? <Redirect to='/shopping-list'/>
@@ -52,7 +50,6 @@ export default function App() {
               }
             </Route>
             <Route exact path="/shopping-list">
-              {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
               {
                 user
                   ? <ListPage />
--- src/ListPage.js
+++ src/ListPage.js
@@ -7,10 +7,15 @@ export default function ListPage() {
   const [listItems, setShoppingList] = useState([]);
 
   // on load, call the fetchItems function (remember: useEffect)
+  useEffect(() => {
+    async function fetchItems() {
+      const items = await getListItems();
+      // fetch the list items and inject them into state
+      setShoppingList(items);
+    }
+    fetchItems();
+  }, [listItems]);
 
-  async function fetchItems() {
-    // fetch the list items and inject them into state
-  }
 
   async function handleDeleteClick() {
     // delete all items
@@ -24,6 +29,9 @@ export default function ListPage() {
       {/* pass fetchItems to the ListItemForm component */}
       <div className='item-list'>
         {/* map through all the list items and render them here */}
+        {
+
+        }
       </div>
 
     </div>
